afec424c44dda4a031195bd0bbff78af3e85b889
Precious265thole <bsc-ele-08-21@unima.ac.mw>
2024-05-07 09:48:45 +0100

    labtask7

---
+++ Lab 7/Shapes/Square.h
@@ -0,0 +1,15 @@
+#pragma once
+
+namespace shapes {
+    class Square {
+    private:
+        double sideLength;
+    public:
+        Square();
+        Square(double length);
+        ~Square();
+        double getSideLength() const;
+    };
+};
+
+
